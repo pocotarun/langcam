3173d23ade1a55e1ac4be4455f0cd129d3a1c999
Tarun <POCO.TARUNDILWAL@GMAIL.COm>
2026-04-01 21:05:58 +0530

    js update & add somthing new!

---
+++ variables.js
@@ -0,0 +1,30 @@
+// let , var , const 
+// "use strict"; forchack hardly 
+const myname = "Tarun";
+let age = 19;
+// display variables
+x = 234;
+console.log(x)
+console.log("MY NAME :", myname);
+console.log("My Age :", agex);
+
+// next year my age changed!
+age = age + 1;
+console.log("MY NEXT YEAR AGE : ", age);
+var agex = 45;
+
+// RAHUL KA DATA
+let namea = "rahul";
+let city = "shamli";
+let phone = '+918765432178'
+
+let addres = `shamli
+uttar pradesh
+india`;
+
+// aase nahi hoga
+// let addresz = 'shamli \n
+// uttar pradesh \n
+// india \n';
+
+console.log(`Candidate Name : ${namea} /n Candidate City : ${city} \n Candidte Addresc : ${addres}`);
